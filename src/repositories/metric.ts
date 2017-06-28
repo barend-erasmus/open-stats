4ce83cd4748b643dc566c5ad17a91cba56182434
Barend Erasmus <developersworkspace@gmail.com>
2017-06-28 08:49:28 +0200

    update

--- src/repositories/metric.ts
+++ src/repositories/metric.ts
@@ -1,5 +1,20 @@
+// Imports metric types
+import { Counter } from './../metric-types/counter';
+
 export class MetricRepository {
+
+    private static counters: Counter[] = [];
+
     constructor() {
         
+    }
+
+    public saveCounter(counter: Counter): Promise<boolean> {
+        MetricRepository.counters.push(counter);
+        return Promise.resolve(true);
+    }
+
+    public listCounters(name: string): Promise<Counter[]> {
+        return Promise.resolve(MetricRepository.counters);
     }
 }
--- src/services/metric.ts
+++ src/services/metric.ts
@@ -1,9 +1,49 @@
 // Imports
 import * as co from 'co';
 
+// Imports repositories
+import { MetricRepository } from './../repositories/metric';
+
+// Import metric types
+import {Counter } from './../metric-types/counter';
+
+// Imports models
+import { Counter as CounterModel } from './../models/counter';
+
+// Imports services
+import { StatsService } from './stats';
+
 export class MetricService {
 
+    private statsService: StatsService = new StatsService();
+
+     constructor(private metricRepository: MetricRepository) {
+
+     }
+
     public log(metric: any): Promise<boolean> {
-        return null;
+
+        if (metric.type === 'counter') {
+            const counter = new Counter(metric.name, metric.value, metric.unit, metric.timestamp);
+            this.metricRepository.saveCounter(counter);
+        } else {
+            throw new Error('Invalid metric type');
+        }
+
+        return Promise.resolve(true);
+    }
+
+    public getCounter(name: string): Promise<CounterModel> {
+        const self = this;
+       return co(function* () {
+         const counters: Counter[] = yield self.metricRepository.listCounters(name);
+         const model: CounterModel = new CounterModel(
+            name,
+            self.statsService.calculateSum(counters.map((x) => x.value)),
+            counters[0].unit
+         );
+
+         return model;
+       });
     }
 }
