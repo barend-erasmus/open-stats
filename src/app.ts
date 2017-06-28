4539a3655cd610426abe5a4c3eb6c15f73d76298
Barend Erasmus <developersworkspace@gmail.com>
2017-06-28 14:57:34 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,13 @@
 // https://github.com/sivy/node-statsd
 
 // Imports
+import * as express from 'express';
+import * as http from 'http';
 import * as co from 'co';
 import * as WebSocket from 'ws';
+
+// Imports middleware
+import * as cors from 'cors';
 
 // Imports services
 import { MetricService } from './services/metric';
@@ -14,19 +19,58 @@ import { MetricRepository } from './repositories/metric';
 
 const metricService = new MetricService(new MetricRepository('mongodb://mongo:27017/open-stats'));
 
-const wss = new WebSocket.Server({ port: 3000, path: '/open-stats' });
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({
+  server: server,
+  path: '/open-stats'
+});
 
 wss.on('connection', (ws: any) => {
   ws.on('message', (message: string) => {
-
 
     const data = JSON.parse(message);
 
     if (data.type === 'test') {
       console.log(message);
     } else {
+      console.log(data.type);
       metricService.log(data);
     }
   });
 });
 
+
+app.get('/counter', (req, res) => {
+  co(function* () {
+    const result = yield metricService.getCounter(req.query.name);
+    res.json(result);
+  });
+});
+
+app.get('/gauge', (req, res) => {
+  co(function* () {
+    const result = yield metricService.getGauge(req.query.name);
+    res.json(result);
+  });
+});
+
+app.get('/sampling', (req, res) => {
+  co(function* () {
+    const result = yield metricService.getSampling(req.query.name);
+    res.json(result);
+  });
+});
+
+app.get('/timing', (req, res) => {
+  co(function* () {
+    const result = yield metricService.getTiming(req.query.name);
+    res.json(result);
+  });
+});
+
+server.listen(3000);
